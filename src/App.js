d5722abba88bdafd074393c4a0f228eea5f3810d
Rafael Martins <rafael.martins92@outlook.com>
2020-07-13 13:58:54 -0300

    Finished Test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState(0);
+  const [value, setValue] = useState("Digite o texto");
+  console.log('clicou!', state)
+  console.log('texto', value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Valor atual: {state}</h1>
+      <button onClick={() => setState(state + 1)}>Somar</button>
+      <br />
+      <br />
+      <h1>Valor do input: {value}</h1>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)}></input>
     </div>
   );
 }
